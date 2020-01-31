c7370c215bd9c8f08ec0d8bd4c985b3aae5dff29
qqri <cqo1154@gmail.com>
2020-01-31 18:15:04 +0900

    weatherapp - geo info 추가

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,11 @@ request({ url:url, json:true },(error,response) => {
     //console.log(response.body.currently)
     console.log(response.body.daily.data[0].summary + "It is currently "+ response.body.currently.temperature + ' degress out. There is a '+ response.body.currently.precipProbability + '% chance of rain')
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicXFxMTIiLCJhIjoiY2s2MXhkZWkzMDhmdjNrcGptenliMTcwMyJ9.ZInL3wI8oVXKSLRBuq_uOA&limit=1'
+
+request({url : geocodeURL , json:true }, (error, response)=>{
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
+}) 
